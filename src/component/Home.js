450333182eedcbd95cc5127c4d89e6abecb68806
DeepakSChouhan <deepak.chouhan@veranda-acacia.com>
2023-03-24 14:14:26 +0530

    FinalCommit

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react';
 import Header from './Header';
 import RestaurantsDisplay from './Restaurants';
 
-const url = "http://3.88.117.174:8443/restaurant/";
+const url = "http://54.81.236.85:8443/restaurant/";
 
 class Home extends Component{
     constructor(){
